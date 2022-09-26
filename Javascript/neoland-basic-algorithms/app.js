f9eed85f60bf82cc105bc42a92916843bd7a7333
Josu Hernandez <josu.hernandez92@gmail.com>
2022-09-26 19:53:06 +0200

    Iteration#3

--- Javascript/neoland-basic-algorithms/app.js
+++ Javascript/neoland-basic-algorithms/app.js
@@ -49,3 +49,16 @@ globalBasePrice = 25000
 car1.finalPrice = car1.basePrice + globalBasePrice
 car2.finalPrice = car2.basePrice + globalBasePrice
 console.log(car1.finalPrice, car2.finalPrice)
+
+// Iteration 3
+console.log(10 * 5)
+console.log(10 / 2)
+console.log(15 % 9)
+const p = 10
+const j = 5
+const o = p + j
+console.log(o)
+const c = 10
+const m = 5
+const i = c * m
+console.log(i)
