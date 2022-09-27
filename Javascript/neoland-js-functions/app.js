93e83e7617ae54fd7c6670850dc4016af7e5b920
Josu Hernandez <josu.hernandez92@gmail.com>
2022-09-27 16:06:55 +0200

    feat: Iteration #1

--- Javascript/neoland-js-functions/app.js
+++ Javascript/neoland-js-functions/app.js
@@ -3,3 +3,20 @@ function sum(a, b) {
   return Math.max(a, b)
 }
 console.log(sum(8, 3))
+
+// Iteration 2
+const avengers = [
+  'Hulk',
+  'Thor',
+  'IronMan',
+  'Captain A.',
+  'Spiderman',
+  'Captain M.',
+]
+function findLongestWord(param) {
+  let longest = ''
+  param.map((word) => (word.length > longest.length ? (longest = word) : ''))
+  return longest
+}
+
+console.log(findLongestWord(avengers))
