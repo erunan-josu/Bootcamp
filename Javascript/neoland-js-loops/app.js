fc2436dbd76ffd3a8a73d35cc733c695d58bf921
Josu Hernandez <josu.hernandez92@gmail.com>
2022-09-28 11:12:02 +0200

    feat: Iteration #7

--- Javascript/neoland-js-loops/app.js
+++ Javascript/neoland-js-loops/app.js
@@ -99,3 +99,19 @@ for (let [i, value] of toys.entries()) {
 }
 
 console.log(newArr)
+
+// Iteration 7
+const popularToys = []
+toys = [
+  { id: 5, name: 'Buzz MyYear', sellCount: 10 },
+  { id: 11, name: 'Action Woman', sellCount: 24 },
+  { id: 23, name: 'Barbie Man', sellCount: 15 },
+  { id: 40, name: 'El gato con Guantes', sellCount: 8 },
+  { id: 40, name: 'El gato felix', sellCount: 35 },
+]
+
+for (let toy of toys) {
+  toy.sellCount > 15 && popularToys.push(toy)
+}
+
+console.log(popularToys)
