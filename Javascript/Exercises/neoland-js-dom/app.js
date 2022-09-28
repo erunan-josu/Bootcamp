d319661273a53663badda61fd64461c6e2504ce0
Josu Hernandez <josu.hernandez92@gmail.com>
2022-09-28 20:57:24 +0200

    feat: Iteration #1

---
+++ Javascript/Exercises/neoland-js-dom/app.js
@@ -0,0 +1,12 @@
+// Iteration 1
+const showMeBtn = document.querySelector('.showme')
+const pillado = document.querySelector('#pillado')
+const paragraphsAll = document.querySelectorAll('p')
+const pokemonAll = document.querySelectorAll('.pokemon')
+const testMeAll = document.querySelectorAll('[data-function=testMe]')
+console.log(showMeBtn)
+console.log(pillado)
+console.log(paragraphsAll.forEach((p) => console.log(p)))
+console.log(pokemonAll.forEach((pokemn) => console.log(pokemn)))
+console.log(testMeAll.forEach((element) => console.log(element)))
+console.log(testMeAll[2])
