10639cbc58b15a4e577e72f8958ecee9258c881b
Josu Hernandez <josu.hernandez92@gmail.com>
2022-10-01 19:28:20 +0200

    restructure folder/files

--- Javascript/Exercises/neoland-js-asynchronous/1-iteration/app.js
+++ Javascript/Exercises/neoland-js-asynchronous/1-iteration/app.js
@@ -59,3 +59,22 @@ const printData = async (text) => {
   })
   console.log(deleteBtn)
 }
+
+// Iteration 2
+
+// 2.1 Convierte la siguiente promesa para esperar a ejecutar el console usando
+// async-await.
+
+const runTimeOut = () => {
+  const promise = new Promise((resolve) => {
+    setTimeout(function () {
+      resolve()
+    }, 2000)
+  })
+
+  promise.then(() => {
+    console.log('Time out!')
+  })
+}
+
+runTimeOut()
