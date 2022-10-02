724ed36cbcca73d14984a271bfd46a0f52125ff0
Josu Hernandez <josu.hernandez92@gmail.com>
2022-10-02 17:07:59 +0200

    feat: Padawan Exercise 2

--- Javascript/Exercises/extra-exercises/1-padawan/exercise-2/app.js
+++ Javascript/Exercises/extra-exercises/1-padawan/exercise-2/app.js
@@ -2,6 +2,7 @@
 
 // Usa un for para remplazar todas las comidas que no sean veganas con las frutas del array
 // de frutas. Recuerda no usar frutas duplicadas. Finalmente, imprime el array resultante.
+
 const fruits = ['Strawberry', 'Banana', 'Orange', 'Apple']
 const foodSchedule = [
   { name: 'Heura', isVegan: true },
