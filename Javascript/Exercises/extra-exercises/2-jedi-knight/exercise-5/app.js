cd810dc21095fbcab237781c43fd73a101136b23
Josu Hernandez <josu.hernandez92@gmail.com>
2022-10-03 23:17:18 +0200

    feat: jedi-knight exercise #5

--- Javascript/Exercises/extra-exercises/2-jedi-knight/exercise-5/app.js
+++ Javascript/Exercises/extra-exercises/2-jedi-knight/exercise-5/app.js
@@ -1,8 +1,9 @@
 // Jedi-Knight - Exercise 5
+
 // 5 - Crea una función llamada rollDice() que reciba como parametro el numero de
-caras que queramos que tenga el dado que deberá simular el codigo dentro de la función.
- Como hemos dicho, que la función use el parametro para simular una tirada de dado
-  y retornar el resultado. 
+// caras que queramos que tenga el dado que deberá simular el codigo dentro de la función.
+// Como hemos dicho, que la función use el parametro para simular una tirada de dado
+// y retornar el resultado.
 const rollDice = (max, min = 1) => {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
