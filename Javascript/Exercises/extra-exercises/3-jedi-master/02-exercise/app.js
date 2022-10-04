953dcaf72648c43e8498053503b652aadf2c2c5c
Josu Hernandez <josu.hernandez92@gmail.com>
2022-10-04 19:27:50 +0200

    feat: jedi-master exercise #2

---
+++ Javascript/Exercises/extra-exercises/3-jedi-master/02-exercise/app.js
@@ -0,0 +1,5 @@
+// Jedi-Master - Exercise 2
+
+// Basandote en el html siguiente, elimina el elemento que tenga la clase .fn-remove-me.
+const removeItem = document.querySelector('.fn-remove-me')
+removeItem.remove()
